854e0a28723baf34aea46db59d170fa192c23f37
shubham-king <84066395+shubham-king@users.noreply.github.com>
2024-12-26 17:51:48 +0530

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,14 @@
+function () {
+  const footerHTML = 
+    <div class="pyr-footer">
+      <p class="note2">Must Join Our <b>Telegram Channel </b> to Watch or Download Animes on Telegram Directly, and Get Updates Regularly <b> &#9989;<a href="https://t.me/flixzilla" style="color:red;"> @flixzilla</a></b> Join this Channel!</p>
+      <hr>
+      <h3 style="text-align: center;"><span style="color:yellow;">Thanks &#10084;&#65039;</span></h3>
+      <p>Thank You For Visiting <span style="color:yellow;"><strong><a style="color:yellow;" href="https://mlwbd.tech">Mlwbd</a></strong></span> The Perfect Spot For HD Animes &amp; Series Download. So Please Keep Downloading &amp; Keep Sharing. Enjoy!</p>
+    </div>
+  ;
+  const container = document.getElementById("pyrFooter");
+  if (container) {
+    container.innerHTML = footerHTML;
+  }
+});
