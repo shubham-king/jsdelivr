83f953cd625bf5688bf74d929c88bb0af382dcb5
shubham-king <84066395+shubham-king@users.noreply.github.com>
2025-01-14 14:13:11 +0530

    Update shortner.js

--- js/shortner.js
+++ js/shortner.js
@@ -1,10 +1,9 @@
-function getDomain(url) {
-    const { hostname } = new URL(url);
-    return hostname;
+function getUrlWithoutProtocol(url) {
+    return url.replace(/^https?:\/\//, ''); // Remove 'http://' or 'https://'
   }
 
   document.getElementById('shortenBtn1').addEventListener('click', function() {
-    const currentUrl = getDomain(window.location.href);
+    const currentUrl = getUrlWithoutProtocol(window.location.href);
     const baseUrl = 'https://linkshortify.com/st?api=62719b77623132400c87e567f8b588dadc9b5205&url=';
     const finalUrl = baseUrl + encodeURIComponent(currentUrl);
 
@@ -13,7 +12,7 @@ function getDomain(url) {
   });
 
   document.getElementById('shortenBtn2').addEventListener('click', function() {
-    const currentUrl = getDomain(window.location.href);
+    const currentUrl = getUrlWithoutProtocol(window.location.href);
     const baseUrl = 'https://arolinks.com/st?api=2527480de7d23ead092b57acf970c96b770c1a6f&url=';
     const finalUrl = baseUrl + encodeURIComponent(currentUrl);
 
