57b326c55e4fc2c8685cb8231ddf27fee200f6d8
shubham-king <84066395+shubham-king@users.noreply.github.com>
2025-01-14 14:07:46 +0530

    Update shortner.js

--- js/shortner.js
+++ js/shortner.js
@@ -1,16 +1,21 @@
- document.getElementById('shortenBtn1').addEventListener('click', function() {
-    const currentUrl = encodeURIComponent(window.location.href);
+function getDomain(url) {
+    const { hostname } = new URL(url);
+    return hostname;
+  }
+
+  document.getElementById('shortenBtn1').addEventListener('click', function() {
+    const currentUrl = getDomain(window.location.href);
     const baseUrl = 'https://linkshortify.com/st?api=62719b77623132400c87e567f8b588dadc9b5205&url=';
-    const finalUrl = baseUrl + currentUrl;
+    const finalUrl = baseUrl + encodeURIComponent(currentUrl);
 
     // Open the shortened URL in a new tab for Skip 1
     window.open(finalUrl, '_blank');
   });
 
   document.getElementById('shortenBtn2').addEventListener('click', function() {
-    const currentUrl = encodeURIComponent(window.location.href);
+    const currentUrl = getDomain(window.location.href);
     const baseUrl = 'https://arolinks.com/st?api=2527480de7d23ead092b57acf970c96b770c1a6f&url=';
-    const finalUrl = baseUrl + currentUrl;
+    const finalUrl = baseUrl + encodeURIComponent(currentUrl);
 
     // Open the shortened URL in a new tab for Skip 2
     window.open(finalUrl, '_blank');
