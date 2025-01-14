e6c96ec586fe7f54a3d6225423641b6d0193b51d
shubham-king <84066395+shubham-king@users.noreply.github.com>
2025-01-14 13:54:26 +0530

    Create shortner.js

---
+++ js/shortner.js
@@ -0,0 +1,17 @@
+ document.getElementById('shortenBtn1').addEventListener('click', function() {
+    const currentUrl = encodeURIComponent(window.location.href);
+    const baseUrl = 'https://linkshortify.com/st?api=62719b77623132400c87e567f8b588dadc9b5205&url=';
+    const finalUrl = baseUrl + currentUrl;
+
+    // Open the shortened URL in a new tab for Skip 1
+    window.open(finalUrl, '_blank');
+  });
+
+  document.getElementById('shortenBtn2').addEventListener('click', function() {
+    const currentUrl = encodeURIComponent(window.location.href);
+    const baseUrl = 'https://arolinks.com/st?api=2527480de7d23ead092b57acf970c96b770c1a6f&url=';
+    const finalUrl = baseUrl + currentUrl;
+
+    // Open the shortened URL in a new tab for Skip 2
+    window.open(finalUrl, '_blank');
+  });
