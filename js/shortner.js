82e38a9d656edffb54e48c6f7aea8af474c8ca4b
shubham-king <84066395+shubham-king@users.noreply.github.com>
2025-01-15 15:45:40 +0530

    Update shortner.js

--- js/shortner.js
+++ js/shortner.js
@@ -1,16 +1,20 @@
- document.getElementById('shortenBtn1').addEventListener('click', function() {
-    const currentUrl = encodeURIComponent(window.location.href);
+function getUrlWithoutProtocol(url) {
+    return url.replace(/^https?:\/\//, ''); // Remove 'http://' or 'https://'
+  }
+
+  document.getElementById('shortenBtn1').addEventListener('click', function() {
+    const currentUrl = getUrlWithoutProtocol(window.location.href);
     const baseUrl = 'https://linkshortify.com/st?api=62719b77623132400c87e567f8b588dadc9b5205&url=';
-    const finalUrl = baseUrl + currentUrl;
+    const finalUrl = baseUrl + encodeURIComponent(currentUrl);
 
     // Open the shortened URL in a new tab for Skip 1
     window.open(finalUrl, '_blank');
   });
 
   document.getElementById('shortenBtn2').addEventListener('click', function() {
-    const currentUrl = encodeURIComponent(window.location.href);
+    const currentUrl = getUrlWithoutProtocol(window.location.href);
     const baseUrl = 'https://modijiurl.com/st?api=ea00541587f4df893ab238587ec4e5c084087172&url=';
-    const finalUrl = baseUrl + currentUrl;
+    const finalUrl = baseUrl + encodeURIComponent(currentUrl);
 
     // Open the shortened URL in a new tab for Skip 2
     window.open(finalUrl, '_blank');
