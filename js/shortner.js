d94aa2249b9000fec2977cb67179538164e3ef28
shubham-king <84066395+shubham-king@users.noreply.github.com>
2025-01-17 16:40:20 +0530

    Update shortner.js

--- js/shortner.js
+++ js/shortner.js
@@ -15,7 +15,7 @@ document.getElementById('shortenBtn2').addEventListener('click', function() {
     const url = new URL(currentUrl); // Create a URL object
     const formattedUrl = url.hostname + url.pathname; // Extract hostname and pathname
 
-    const baseUrl = 'https://linkshortify.com/st?api=62719b77623132400c87e567f8b588dadc9b5205&url=';
+    const baseUrl = 'https://arolinks.com/st?api=2527480de7d23ead092b57acf970c96b770c1a6f&url=';
     const finalUrl = baseUrl + formattedUrl;
 
     // Open the shortened URL in a new tab for Skip 2
